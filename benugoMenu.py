ad3f39dd452f3937dfd401ccb03e7fa85e647164
Analytical Services <analyticalservices@Andys-MacBook-Air.local>
2017-02-13 14:34:02 +0000

    Function to parse Benugo menu from intranet.  Requires pptx package.

---
+++ benugoMenu.py
@@ -0,0 +1,21 @@
+import time, pptx, urllib.request
+
+def menu(day = None):
+	if day == None:
+		day = time.strftime('%A')
+	urllib.request.urlretrieve("https://intranet.justice.gov.uk/documents/2016/02/benugo-weekly-menu.pptx", "bwm1.pptx")
+	prs = pptx.Presentation('bwm1.pptx')
+	text_runs = []
+	for slide in prs.slides:
+		for shape in slide.shapes:
+			if not shape.has_text_frame:
+				continue
+			for paragraph in shape.text_frame.paragraphs:
+				par = []
+				for run in paragraph.runs:
+					par.append(run.text.lower())
+				text_runs.append(par)
+	return text_runs;
+# print(text_runs)
+
+
