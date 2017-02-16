29e4373e195b84768be80426f2462a8a89d6bc07
Ross Wyatt <rosswyatt@Ross-Wyatt-Mac.local>
2017-02-16 11:03:49 +0000

    Update allocation

--- TasksAllocate.py
+++ TasksAllocate.py
@@ -1,12 +1,13 @@
 import random
 
 def shitty_task(command):
-	if command == "below 7":
+	if command[10:17] == "below 7":
 		members = ["Dan J", "Dan H", "Karik", "George", "Hayden", "Ben", "Vicky", "Andy"]
 		print("Hello")
-	elif command == "exclude jon":
+	elif command[10:21] == "exclude jon":
 		members =  ["Sam", "Robin", "Ross", "Dan J", "Dan H", "Karik", "George", "Hayden", "Ben", "Vicky", "Andy"]
 	else:
 		members = ["Jon", "Sam", "Robin", "Ross", "Dan J", "Dan H", "Karik", "George", "Hayden", "Ben", "Vicky", "Andy"]
 
 	return random.choice(members)
+
