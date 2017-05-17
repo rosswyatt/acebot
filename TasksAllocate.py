47d9cfa211252c60fe3ef1414c43f0948f8739aa
Ross Wyatt <rosswyatt@ross-wyatt-mac.home>
2017-05-17 12:52:52 +0100

    Update team

--- TasksAllocate.py
+++ TasksAllocate.py
@@ -2,10 +2,10 @@ import random
 
 def shitty_task(command):
 	if command[9:16] == "below 7":
-		members = ["@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton"]
+		members = ["@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton", "@marwa_el1", "@rossk", "@sam_lindsay"]
 	elif command[9:20] == "exclude jon":
-		members =  ["@samtazzyman", "@robin_linacre", "@ross_wyatt", "@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton"]
+		members =  ["@samtazzyman", "@robin_linacre", "@ross_wyatt", "@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton", "@marwa_el1", "@olivia_lewis", "@rossk", "@sam_lindsay"]
 	else:
-		members = ["@jonroberts", "@samtazzyman", "@robin_linacre", "@ross_wyatt", "@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton"]
+		members = ["@jonroberts", "@samtazzyman", "@robin_linacre", "@ross_wyatt", "@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton", "@marwa_el1", "@olivia_lewis", "@rossk", "@sam_lindsay"]
 
 	return "The winner is <" + random.choice(members) + ">"
