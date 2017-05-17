41c40f8c373877089e6cb053c330cca56bb2ee7e
Ross Wyatt <rosswyatt@ross-wyatt-mac.home>
2017-05-17 13:14:17 +0100

    update team

--- TasksAllocate.py
+++ TasksAllocate.py
@@ -8,4 +8,5 @@ def shitty_task(command):
 	else:
 		members = ["@jonroberts", "@samtazzyman", "@robin_linacre", "@ross_wyatt", "@danjones", "@daniel_hills", "@karik.isichei", "@gkelly", "@hayden_sansum", "@bfortescue", "@vickyhughes", "@apoulton", "@marwa_el1", "@olivia_lewis", "@rossk", "@sam_lindsay"]
 
+
 	return "The winner is <" + random.choice(members) + ">"
